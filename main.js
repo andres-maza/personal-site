ec687af7e78926922c63c63c12b2ec6684a17307
Andres Maza <andres.maza@barrelny.com>
2019-09-01 02:23:01 -0400

    Add logic to deal with gradient bg glitch. Add fade in animation. Cleaned up code.

--- main.js
+++ main.js
@@ -2,6 +2,13 @@ document.addEventListener('DOMContentLoaded', function(){
   // If/else mobile detection credit to: https://stackoverflow.com/a/3540295
   // Canvas erase effect credit to: https://codepen.io/karlovidek/pen/eGGvMb
 
+  /*
+  // Not 100% sure if this is doing anything to help touchstart issues on mobile devices...
+  if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
+  || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) {
+    document.querySelector('#canvas-overlay').style.cursor = 'pointer';
+    document.querySelector('#canvas-lines').style.cursor = 'pointer';
+  }*/
 
   // Get the #canvas-overlay and #canvas-lines canvas elements and set getContext to 2d
   var canvas = document.querySelector('#canvas-overlay');
@@ -13,9 +20,9 @@ document.addEventListener('DOMContentLoaded', function(){
   // Empty array to be populated on mousemove/touch events, see onMouseMove, onTouchStart, onTouchMove.
   var points = [];
 
-  //Set color of #canvas-overlay
+  // Set color of canvas rectangle for #canvas-overlay
   canvasContext.fillStyle="rgba(255, 255, 255, 1)";
-  //Draw rectangle for #canvas-overlay at 0x 0y using the width and height of #canvas-overlay
+  // Draw canvas rectangle for #canvas-overlay
   canvasContext.fillRect(0, 0, canvas.width, canvas.height);
 
   // Set event listeners for mousemove and window resize.
@@ -27,26 +34,32 @@ document.addEventListener('DOMContentLoaded', function(){
     window.addEventListener('orientationchange', resizeCanvases);
     resizeCanvases();
     tick();
-
-    if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
-    || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) {
-      document.querySelector('#canvas-overlay').style.cursor = 'pointer';
-      document.querySelector('#canvas-lines').style.cursor = 'pointer';
-    }
   }
 
   init();
 
-  // Reset the size of canvas on screen resize.
+  // Reset the size of each canvas on scren resize or device orientation.
   function resizeCanvases() {
-    //The width of #canvas-overlay is equal to the width of #canvas-lines which is equal to the width of the window
+    // Set display of #canvas-overlay to none, to prevent weird flashing of gradient background.
+    document.querySelector('#canvas-overlay').style.display = 'none';
+    // The width of both canvases is equal to the width of the user's browser window.
     canvas.width = lineCanvas.width = window.innerWidth;
-    //The height of #canvas-overlay is equal to the height of #canvas-lines which is equal to the height of the window
+    // The height of both canvases is equal to the height of the user's browser window.
     canvas.height = lineCanvas.height = window.innerHeight;
   }
 
-  // Handle mousemvoe events.
+  // Reset display of #canvas-overlay to visible on mouse movement, or touch events.
+  function checkCanvasDisplay() {
+    var isDisplayed = document.querySelector('#canvas-overlay').style.display;
+    if(isDisplayed != 'block'){
+      document.querySelector('#canvas-overlay').style.display = 'block';
+    }
+    return;
+  }
+
+  // Handle mousemove event.
   function onMouseMove(event) {
+    checkCanvasDisplay();
     // Add into "points" array a new object everytime a mousemove event is detected, with the following: time, x position, y position.
     points.push({
       // Time is needed for requestAnimationFrame(?). The points here are used to create previous and new paths.
@@ -56,19 +69,20 @@ document.addEventListener('DOMContentLoaded', function(){
     });
   }
 
+  // Touchstart event that might be working...? Works on Chrome at least.
   function onTouchStart(event) {
+    checkCanvasDisplay();
     event.preventDefault();
     points.push({
       time: Date.now(),
       x: event.touches[0].clientX,
       y: event.touches[0].clientY
     });
-
-    console.log(points);
   }
 
   // Handle touchmove events for mobile/touch devices.
   function onTouchMove(event) {
+    checkCanvasDisplay();
     points.push({
       time: Date.now(),
       x: event.touches[0].clientX,
@@ -98,31 +112,34 @@ document.addEventListener('DOMContentLoaded', function(){
     lineCanvasContext.shadowBlur = 10;
     // Set the color of the shadow.
     lineCanvasContext.shadowColor = '#000';
+    // Set the width of the canvas' line.
     lineCanvasContext.lineWidth = 400;
+    // Set the height of the canvas' line (prevents distortion when using screen.width/height).
     lineCanvasContext.lineHeight = 400;
 
-    //Clear a rectangle the size of the window
+    // First thing: reset the canvas area to blank.
     lineCanvasContext.clearRect(0, 0, lineCanvas.width, lineCanvas.height);
 
-    //For
-    //If the lenght of the points array is greater than "i" (default set at 1) then add one to "i"
+    // For
+    // If the length of array items in "points" is larger than "i", then add 1 to "i".
     for (var i = 1; i < points.length; i++) {
-      //"point" is equal to each indiviudal object in the points array
+      // Get each individual "point".
       var point = points[i];
-      //"previous point" is equal to each individual boject in the points array -1 (so one before "points")
+      // Get the previous/last "point".
       var previousPoint = points[i - 1];
 
-      // Fade points as they age
+      // Fade points as they age.
       var age = Date.now() - point.time;
       var opacity = (pointLifetime - age * 0) / pointLifetime;
       lineCanvasContext.strokeStyle = 'rgba(0, 0, 0, ' + opacity + ')';
-      //Create a new path
+
+      // Create a new path.
       lineCanvasContext.beginPath();
-      //Move the new path to be at the previous point x and y
+      // Move the new path to be at the previous point x and y.
       lineCanvasContext.moveTo(previousPoint.x, previousPoint.y);
-      //Create a line from current piont x and y
+      // Create a line from current point x and y.
       lineCanvasContext.lineTo(point.x, point.y);
-      //Draw a stroke on the path using the present strokeStyle (rgba(0,0,0,1))
+      // Draw a stroke on the path using the present strokeStyle (rgba(0,0,0,1)).
       lineCanvasContext.stroke();
     }
   }
